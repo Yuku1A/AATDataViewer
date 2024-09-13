193671996b315c7d71bda9db59465b8dfac965ba
Yuku <54231633+Yuku1A@users.noreply.github.com>
2024-09-14 00:39:59 +0900

    FileUtilをTypeScript化

--- src/Util/FileUtil.ts
+++ src/Util/FileUtil.ts
@@ -1,10 +1,10 @@
 import * as yaml from "js-yaml"
 
-export async function readTextFile(file) {
+export async function readTextFile(file: File) {
   return await file.text();
 }
 
-export async function readYamlFile(file) {
+export async function readYamlFile(file: File) {
   const text = await readTextFile(file);
   return yaml.load(text);
 }
