0a8bcd19dfd446fe24df49d28859e27edb34d4c6
Yuku <54231633+Yuku1A@users.noreply.github.com>
2024-09-14 19:11:44 +0900

    JSDocを追加

--- src/Util/UnitUtil.js
+++ src/Util/UnitUtil.js
@@ -3,20 +3,40 @@ import duration from "dayjs/plugin/duration"
 
 dayjs.extend(duration);
 
+/**
+ * 
+ * @param {string} milliTimeStr ミリ秒の整数のstring
+ * @returns {duration.Duration}
+ */
 export function MilliTimeStrToDuration(milliTimeStr) {
   const milliTimeInt = parseInt(milliTimeStr);
   return dayjs.duration(milliTimeInt);
 }
 
+/**
+ * 
+ * @param {duration.Duration} dayjsTime 
+ * @returns {string}
+ */
 export function DayjsTimeToHHmmss(dayjsTime) {
   return dayjsTime.format("HH:mm:ss");
 }
 
+/**
+ * 
+ * @param {string} milliTimeStr 
+ * @returns {string}
+ */
 export function MilliTimeStrToHHmmss(milliTimeStr) {
   const dayjsTime = MilliTimeStrToDuration(milliTimeStr);
   return DayjsTimeToHHmmss(dayjsTime);
 }
 
+/**
+ * 
+ * @param {string} HHmmssStr 
+ * @returns {duration.Duration}
+ */
 export function HHmmssToDuration(HHmmssStr) {
   const HHmmssArray = HHmmssStr.split(":");
   return dayjs.duration({
@@ -26,6 +46,11 @@ export function HHmmssToDuration(HHmmssStr) {
   })
 }
 
+/**
+ * 
+ * @param {string} testStr 
+ * @returns {boolean}
+ */
 export function isStringHHmmss(testStr) {
   const regex = new RegExp("^\\d\\d:[0-5]\\d:[0-5]\\d$");
   return regex.test(testStr);
